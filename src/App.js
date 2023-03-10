1fe44ebda5bf2b3a5d2daffc0af982759599acba
Jonathan Hawes <jhawes580@gmail.com>
2023-03-10 00:44:24 -0800

    Install react-router and create component files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import Home from './Home';
+import Shop from './Shop';
+import Cart from './Cart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/shop" element={Shop} />
+        <Route path="/cart" element={Cart} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import Nav from './Nav';
+im;
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <div></div>
+    </>
+  );
+}
